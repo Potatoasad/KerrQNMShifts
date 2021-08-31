0aa4b98b4b6ad69c13f7ef843201ed924afc4d15
Asad Hussain <asadh@utexas.edu>
2021-08-31 08:27:22 -0500

    fixed qnm interoperability

--- src/KerrQNMShifts.jl
+++ src/KerrQNMShifts.jl
@@ -118,7 +118,7 @@ end
 
 qnm = pyimport("qnm")
 qnm.download_data()
-function qnmfunctionnew(s,l,m,n,a)
+function qnmfunctionnew(s,l,m,n,a; qnm=qnm)
     grav_freq = qnm.modes_cache(s=s,l=l,m=m,n=n)
     ω, Alm, Cllʼ = grav_freq(a=a)
     qnmfunction(Custom; s=s,l=l,m=m,n=n,a=a,ω=ω,Alm=Alm,Cllʼ=Cllʼ)
